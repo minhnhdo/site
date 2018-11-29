24d2eec8253aaf80c1f16a9687cf0f83a8a0a273
Do Nhat Minh <mrordinaire@gmail.com>
2018-11-29 05:41:38 -0800

    fixes

--- site.hs
+++ site.hs
@@ -2,7 +2,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import       Control.Monad (liftM)
-import       Data.Monoid (mappend)
+import       Data.Monoid ((<>))
 import       Data.Text.Lazy (unpack)
 import       Clay (Css, compact, renderWith)
 import       Hakyll
@@ -14,7 +14,7 @@ import       DefaultCss (defaultCss)
 main :: IO ()
 main = hakyll $ do
   match "resume.pdf" $ do
-    route   idRoute
+    route idRoute
     compile copyFileCompiler
 
   create ["CNAME"] $ do
@@ -25,23 +25,23 @@ main = hakyll $ do
     route idRoute
     compile $ compileWithClay defaultCss
 
-  tags <- buildTags "posts/*" (fromCapture "tags/*.html")
+  tags <- buildTags postsPattern (fromCapture "tags/*.html")
 
-  match "posts/*.mkd" $ do
+  match postsPattern $ do
     route $ setExtension "html"
     compile $ pandocCompiler
-      >>= loadAndApplyTemplate "_templates/post.html" (postCtxWithTags tags)
-      >>= loadAndApplyTemplate "_templates/default.html" postCtx
-      >>= relativizeUrls
+          >>= loadAndApplyTemplate "_templates/post.html" (postCtxWithTags tags)
+          >>= loadAndApplyTemplate "_templates/default.html" postCtx
+          >>= relativizeUrls
 
   tagsRules tags $ \tag pattern -> do
     let title = "Posts tagged \"" ++ tag ++ "\""
     route idRoute
     compile $ do
       posts <- recentFirst =<< loadAll pattern
-      let ctx = constField "title" title `mappend`
-                listField "posts" postCtx (return posts) `mappend`
-                defaultContext
+      let ctx = constField "title" title
+             <> listField "posts" postCtx (return posts)
+             <> defaultContext
       makeItem ""
         >>= loadAndApplyTemplate "_templates/tag.html" ctx
         >>= loadAndApplyTemplate "_templates/default.html" ctx
@@ -50,10 +50,9 @@ main = hakyll $ do
   create ["archive.html"] $ do
     route idRoute
     compile $ do
-      let archiveCtx =
-            listField "posts" postCtx allPosts `mappend`
-            constField "title" "Archives"      `mappend`
-            defaultContext
+      let archiveCtx = listField "posts" postCtx allPosts
+                    <> constField "title" "Archives"
+                    <> defaultContext
 
       makeItem ""
         >>= loadAndApplyTemplate "_templates/archive.html" archiveCtx
@@ -62,11 +61,10 @@ main = hakyll $ do
 
 
   match "index.mkd" $ do
-    route   $ setExtension "html"
+    route $ setExtension "html"
     compile $ do
-      let indexCtx =
-            listField "posts" postCtx (liftM (take 5) allPosts) `mappend`
-            defaultContext
+      let indexCtx = listField "posts" postCtx (liftM (take 5) allPosts)
+                  <> defaultContext
 
       getResourceBody
         >>= applyAsTemplate indexCtx
@@ -77,14 +75,17 @@ main = hakyll $ do
   match "_templates/*.html" $ compile templateCompiler
 
 --------------------------------------------------------------------------------
+postsPattern :: Pattern
+postsPattern = "posts/**.mkd"
+
 allPosts :: Compiler [Item String]
-allPosts = recentFirst =<< loadAll "posts/*"
+allPosts = recentFirst =<< loadAll postsPattern
 
 postCtx :: Context String
-postCtx = dateField "date" "%B %e, %Y" `mappend` defaultContext
+postCtx = dateField "date" "%B %e, %Y" <> defaultContext
 
 postCtxWithTags :: Tags -> Context String
-postCtxWithTags tags = tagsField "tags" tags `mappend` defaultContext
+postCtxWithTags tags = tagsField "tags" tags <> defaultContext
 
 compileWithClay :: Css -> Compiler (Item String)
 compileWithClay = makeItem . unpack . renderWith compact []

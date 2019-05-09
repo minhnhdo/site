adab9aa20db7add7b8f88c8594597890fd17d634
Do Nhat Minh <minhnhdo@gmail.com>
2019-05-09 10:03:49 -0700

    migrated to minhnhdo.github.io

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
 
 stack exec site clean && stack exec site build &&
-  rsync -avz ../mrordinaire.github.io/.git _site/ &&
-  rsync -avz --delete _site/ ../mrordinaire.github.io &&
-  echo $(cd ../mrordinaire.github.io &&
+  rsync -avz ../minhnhdo.github.io/.git _site/ &&
+  rsync -avz --delete _site/ ../minhnhdo.github.io &&
+  echo $(cd ../minhnhdo.github.io &&
          git add -A &&
          git commit -m "updated at `date +%Y-%m-%dT%H:%M:%S`" &&
          git push origin master)
